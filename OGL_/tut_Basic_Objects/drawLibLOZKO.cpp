b9fd3f0289cc0b0bc3781d62a59297e8008d6e77
Renceex <71285567+Renceex@users.noreply.github.com>
2021-12-15 16:32:03 +0300

    Add files via upload

--- OGL_/tut_Basic_Objects/drawLibLOZKO.cpp
+++ OGL_/tut_Basic_Objects/drawLibLOZKO.cpp
@@ -1,4 +1,4 @@
-#include"Header.h"
+#include"drawLibLOZKO.h"
 
 
 
@@ -24,10 +24,10 @@ void drawGrafik() {
     glEnd();
 
 }
-void drawCat() {
-    glTranslatef(-.85, .1, .5);
+void drawCat(Figra fig) {
+    glTranslatef(0,0,0);
     glBegin(GL_TRIANGLES);//хвостик
-    glColor3f(.4, -.1, .14);
+    glColor3f(fig.color[0], fig.color[1], fig.color[2]);
     glVertex2f(-.2, -.3);
     glVertex2f(.4, .0);
     glVertex2f(.55, -.02);
@@ -145,12 +145,11 @@ void drawDog() {
 }
 
 
-void drawSpruce(float x, float y, float z) {
+void drawSpruce() {
     glPushMatrix();
-    glTranslatef(x, y, z);
+    //glTranslatef(fig.x, fig.y, fig.z);
     glBegin(GL_TRIANGLES);
     //верх елки
-    glColor3f(0, 1, .0);
     glVertex2f(-.9, 2.2);
     glColor3f(0, 1, .0);
     glVertex2f(-.7, 2.2);
@@ -186,3 +185,180 @@ void drawSpruce(float x, float y, float z) {
     glEnd();
     glPopMatrix();
 }
+/*
+   glBegin(GL_QUADS);
+
+    //Задний план
+   glColor3f(.8, .8, .0);
+   glVertex2f(2, 2);
+   glColor3f(.02, .02, .79);
+   glVertex2f(2, -2);
+   glVertex2f(-2, -2);
+   glVertex2f(-2, 2);
+   // земля
+   glColor3f(.0, .72, .0);
+   glVertex2f(1, -.5);
+   glColor3f(.0, .5, .0);
+   glVertex2f(-1, -.5);
+   glColor3f(.0, .5, .0);
+   glVertex2f(-1, -1);
+   glColor3f(.0, .5, .0);
+   glVertex2f(1, -1);
+   //дорога
+   glColor3f(.0, .0, .0);
+   glVertex2f(1, -.5);
+   glColor3f(.0, .0, .0);
+   glVertex2f(-1, -.5);
+   glColor3f(.0, .0, .0);
+   glVertex2f(-1, -.2);
+   glColor3f(.0, .0, .0);
+   glVertex2f(1, -.2);
+   //разметка на дороге
+   glColor3f(.0, .0, .0);
+   glVertex2f(1, -.5);
+   glColor3f(.0, .0, .0);
+   glVertex2f(-1, -.5);
+   glColor3f(.0, .0, .0);
+   glVertex2f(-1, -.2);
+   glColor3f(.0, .0, .0);
+   glVertex2f(1, -.2);
+   //следующая линия
+   glColor3f(.0, .0, .0);
+   glVertex2f(1, -.5);
+   glColor3f(.0, .0, .0);
+   glVertex2f(-1, -.5);
+   glColor3f(.0, .0, .0);
+   glVertex2f(-1, -.2);
+   glColor3f(.0, .0, .0);
+   glVertex2f(1, -.2);
+   glEnd();
+
+
+   //кораблик
+   glBegin(GL_QUADS);
+   //корпус коробля
+   glColor3f(.5, .2, .0);
+   glVertex2f(.2, .6);
+   glVertex2f(.93, .6);
+   glVertex2f(.78, .44);
+   glVertex2f(.35, .44);
+   //мачта
+   glColor3f(.5, .2, .0);
+   glVertex2f(.55, .97);
+   glVertex2f(.55, .6);
+   glVertex2f(.59, .6);
+   glVertex2f(.59, .97);
+   glEnd();
+   glBegin(GL_TRIANGLES);
+   //флаг 1
+   glColor3f(.9, .9, .9);
+   glVertex2f(.59, .97);
+   glVertex2f(.59, .6);
+   glVertex2f(.71, .62);
+   //флаг 2
+   glColor3f(.5, .9, .9);
+   glVertex2f(.55, .97);
+   glVertex2f(.55, .6);
+   glVertex2f(.32, .70);
+   glEnd();
+
+
+   //часть дома
+   glBegin(GL_QUADS);
+   //труба дома
+   glColor3f(.9, .9, .9);
+   glVertex2f(-.42, .5);
+   glColor3f(.5, .5, .5);
+   glVertex2f(-.42, .0);
+   glColor3f(.9, .9, .9);
+   glVertex2f(-.3, .0);
+   glVertex2f(-.3, .5);
+   glEnd();
+
+
+   //Дом
+   glScalef(1.3, 1.3, 1.3);
+   glTranslatef(-.2, .0, .0);
+   glBegin(GL_TRIANGLES);
+   // стены 1
+   glColor3f(.9, .9, .9);
+   glVertex2f(.0, .0);
+   glVertex2f(-.5, .0);
+   glVertex2f(.0, -.5);
+   // стены 2
+   glColor3f(.5, .5, .5);
+   glVertex2f(-.5, -.5);
+   glColor3f(.9, .9, .9);
+   glVertex2f(-.5, .0);
+   glVertex2f(.0, -.5);
+   //крыша
+   glColor3f(.5, .1, .1);
+   glVertex2f(-.6, .0);
+   glColor3f(.9, .5, .5);
+   glVertex2f(-.25, .3);
+   glVertex2f(.1, .0);
+   glEnd();
+   glTranslatef(.0, -.23, .0);
+   glBegin(GL_QUADS);
+   // Дверь дома
+   glColor3f(.9, .0, .0);
+   glVertex2f(-.1, -.27);
+   glVertex2f(-.22, -.27);
+   glVertex2f(-.22, .0);
+   glVertex2f(-.1, .0);
+   // Окно дома
+   glColor3f(.8, .8, .4);
+   glVertex2f(-.43, -.05);
+   glVertex2f(-.43, .1);
+   glVertex2f(-.28, .1);
+   glVertex2f(-.28, -.05);
+   // рама окна
+   glColor3f(1, 1, 1);
+   glVertex2f(-.42, -.04);
+   glVertex2f(-.42, .09);
+   glVertex2f(-.29, .09);
+   glVertex2f(-.29, -.04);
+   glEnd();
+
+
+   // Машина
+   glScalef(0.8, 0.8, 0);
+   glBegin(GL_QUADS);
+   //колесо Л
+   glColor3f(.11, .93, .41);
+   glVertex2f(.68, .04);
+   glVertex2f(.76, -.04);
+   glVertex2f(.68, -.12);
+   glVertex2f(.60, -.04);
+   //колесо П
+   glColor3f(.21, .67, .84);
+   glVertex2f(.3, .04);
+   glVertex2f(.38, -.04);
+   glVertex2f(.3, -.12);
+   glVertex2f(.22, -.04);
+   //Корпус
+   glColor3f(.71, .7, .71);
+   glVertex2f(.2, .0);
+   glVertex2f(.78, .0);
+   glVertex2f(.78, .24);
+   glVertex2f(.2, .24);
+   //Бампер машины
+   glColor3f(.71, .7, .71);
+   glVertex2f(.90, .0);
+   glVertex2f(.78, .0);
+   glVertex2f(.78, .13);
+   glVertex2f(.90, .13);
+   //окно машины
+   glColor3f(.3, .3, .3);
+   glVertex2f(.67, .23);
+   glVertex2f(.67, .14);
+   glVertex2f(.76, .14);
+   glVertex2f(.76, .23);
+
+   glEnd();
+
+   glBegin(GL_TRIANGLES);//на запас
+
+   //
+   glEnd();
+   */
--- OGL_/tut_Basic_Objects/drawLibLOZKO.h
+++ OGL_/tut_Basic_Objects/drawLibLOZKO.h
@@ -1,1 +1,14 @@
 #pragma once
+#include"GL/freeglut.h"
+#define _USE_MATH_DEFINES
+#include"cmath"
+struct Figra {
+	float x, y, z;
+	float color[3];
+};
+void drawGrafik();
+void drawCat(Figra fig);
+void drawDog();
+void drawSpruce();
+void DrawCat(float x, float y, bool IsRight, float a);
+
